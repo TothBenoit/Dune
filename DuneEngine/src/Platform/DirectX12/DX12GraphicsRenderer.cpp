bff2976b971f5ddfce75490ff2282062d58ad098
Benoit Toth <Benoit.tothdenima@gmail.com>
2022-04-27 19:45:16 +0200

    Remove D3DX12 which was hidding some descriptor initialization which I didn't want for the POC. Initialized scissorRect and viewport (triangle wasn't drawn because of this)

--- DuneEngine/src/Platform/DirectX12/DX12GraphicsRenderer.cpp
+++ DuneEngine/src/Platform/DirectX12/DX12GraphicsRenderer.cpp
@@ -2,9 +2,7 @@
 #include "Platform/DirectX12/DX12GraphicsRenderer.h"
 #include "Dune/Core/Logger.h"
 
-
 #include <dxgidebug.h>
-#include "Includes/d3dx12.h"
 #include <d3dcompiler.h>
 
 namespace Dune
@@ -49,8 +47,39 @@ namespace Dune
 			psoDesc.pRootSignature = m_rootSignature.Get();
 			psoDesc.VS = { reinterpret_cast<UINT8*>(vertexShader->GetBufferPointer()), vertexShader->GetBufferSize() };
 			psoDesc.PS = { reinterpret_cast<UINT8*>(pixelShader->GetBufferPointer()), pixelShader->GetBufferSize() };
-			psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-			psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+			D3D12_RASTERIZER_DESC rasterDesc;
+			rasterDesc.FillMode = D3D12_FILL_MODE_SOLID;
+			rasterDesc.CullMode = D3D12_CULL_MODE_NONE;
+			rasterDesc.FrontCounterClockwise = TRUE;
+			rasterDesc.DepthBias = D3D12_DEFAULT_DEPTH_BIAS;
+			rasterDesc.DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
+			rasterDesc.SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
+			rasterDesc.DepthClipEnable = FALSE;
+			rasterDesc.MultisampleEnable = FALSE;
+			rasterDesc.AntialiasedLineEnable = FALSE;
+			rasterDesc.ForcedSampleCount = 0;
+			rasterDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+			psoDesc.RasterizerState = rasterDesc;
+			psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+			D3D12_BLEND_DESC blendDesc;
+			blendDesc.AlphaToCoverageEnable = FALSE;
+			blendDesc.IndependentBlendEnable = FALSE;
+			const D3D12_RENDER_TARGET_BLEND_DESC defaultRenderTargetBlendDesc = {
+				FALSE,
+				FALSE,
+				D3D12_BLEND_ONE,
+				D3D12_BLEND_ZERO,
+				D3D12_BLEND_OP_ADD,
+				D3D12_BLEND_ONE,
+				D3D12_BLEND_ZERO,
+				D3D12_BLEND_OP_ADD,
+				D3D12_LOGIC_OP_NOOP,
+				D3D12_COLOR_WRITE_ENABLE_ALL,
+			};
+			for (UINT i = 0; i < D3D12_SIMULTANEOUS_RENDER_TARGET_COUNT; ++i)
+				blendDesc.RenderTarget[i] = defaultRenderTargetBlendDesc;
+			psoDesc.BlendState = blendDesc;
+
 			psoDesc.DepthStencilState.DepthEnable = FALSE;
 			psoDesc.DepthStencilState.StencilEnable = FALSE;
 			psoDesc.SampleMask = UINT_MAX;
@@ -84,19 +113,41 @@ namespace Dune
 			// recommended. Every time the GPU needs it, the upload heap will be marshalled 
 			// over. Please read up on Default Heap usage. An upload heap is used here for 
 			// code simplicity and because there are very few verts to actually transfer.
-			CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
-			auto desc = CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize);
+			D3D12_HEAP_PROPERTIES heapProps;
+			heapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+			heapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+			heapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+			heapProps.CreationNodeMask = 1;
+			heapProps.VisibleNodeMask = 1;
+
+			D3D12_RESOURCE_DESC vertexBufferResourceDesc;
+			vertexBufferResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+			vertexBufferResourceDesc.Alignment = 0;
+			vertexBufferResourceDesc.Width = vertexBufferSize;
+			vertexBufferResourceDesc.Height = 1;
+			vertexBufferResourceDesc.DepthOrArraySize = 1;
+			vertexBufferResourceDesc.MipLevels = 1;
+			vertexBufferResourceDesc.Format = DXGI_FORMAT_UNKNOWN;
+			vertexBufferResourceDesc.SampleDesc.Count = 1;
+			vertexBufferResourceDesc.SampleDesc.Quality = 0;
+			vertexBufferResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+			vertexBufferResourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
 			ThrowIfFailed(m_device->CreateCommittedResource(
 				&heapProps,
 				D3D12_HEAP_FLAG_NONE,
-				&desc,
+				&vertexBufferResourceDesc,
 				D3D12_RESOURCE_STATE_GENERIC_READ,
 				nullptr,
 				IID_PPV_ARGS(&m_vertexBuffer)));
 
 			// Copy the triangle data to the vertex buffer.
 			UINT8* pVertexDataBegin;
-			CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+
+			// We do not intend to read from this resource on the CPU.
+			D3D12_RANGE readRange;  
+			readRange.Begin = 0;
+			readRange.End = 0;    
 			ThrowIfFailed(m_vertexBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pVertexDataBegin)));
 			memcpy(pVertexDataBegin, triangleVertices, sizeof(triangleVertices));
 			m_vertexBuffer->Unmap(0, nullptr);
@@ -238,6 +289,19 @@ namespace Dune
 
 	void DX12GraphicsRenderer::CreateSwapChain(HWND handle)
 	{
+		m_scissorRect.left = 0;
+		m_scissorRect.top = 0;
+		m_scissorRect.right = static_cast<LONG>(1600);
+		m_scissorRect.bottom = static_cast<LONG>(900);
+
+		m_viewport.TopLeftX = 0.0f;
+		m_viewport.TopLeftY = 0.0f;
+		m_viewport.Width = static_cast<float>(1600);
+		m_viewport.Height = static_cast<float>(900);
+		m_viewport.MinDepth = .1f;
+		m_viewport.MaxDepth = 1000.f;
+
+
 		ThrowIfFailed(m_factory->MakeWindowAssociation(handle, DXGI_MWA_NO_ALT_ENTER));
 
 		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
@@ -265,6 +329,7 @@ namespace Dune
 
 	void DX12GraphicsRenderer::CreateRenderTargets()
 	{
+
 		// Create descriptor heaps.
 		{
 			// Describe and create a render target view (RTV) descriptor heap.
@@ -298,20 +363,42 @@ namespace Dune
 
 	void DX12GraphicsRenderer::CreateRootSignature()
 	{
-		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
-		rootSignatureDesc.Init(0, nullptr, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+		D3D12_DESCRIPTOR_RANGE1 ranges[1];
+		ranges[0].BaseShaderRegister = 0;
+		ranges[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
+		ranges[0].NumDescriptors = 1;
+		ranges[0].RegisterSpace = 0;
+		ranges[0].OffsetInDescriptorsFromTableStart = 0;
+		ranges[0].Flags = D3D12_DESCRIPTOR_RANGE_FLAG_NONE;
+
+		D3D12_ROOT_PARAMETER1 rootParameters[1];
+		rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+		rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+
+		rootParameters[0].DescriptorTable.NumDescriptorRanges = 1;
+		rootParameters[0].DescriptorTable.pDescriptorRanges = ranges;
+
+		D3D12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Version = D3D_ROOT_SIGNATURE_VERSION_1_1;
+		rootSignatureDesc.Desc_1_1.Flags =
+			D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+		rootSignatureDesc.Desc_1_1.NumParameters = 1;
+		rootSignatureDesc.Desc_1_1.pParameters = rootParameters;
+		rootSignatureDesc.Desc_1_1.NumStaticSamplers = 0;
+		rootSignatureDesc.Desc_1_1.pStaticSamplers = nullptr;
 
 		Microsoft::WRL::ComPtr<ID3DBlob> signature;
 		Microsoft::WRL::ComPtr<ID3DBlob> error;
-		ThrowIfFailed(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+		ThrowIfFailed(D3D12SerializeVersionedRootSignature(&rootSignatureDesc, &signature, &error));
 		ThrowIfFailed(m_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_rootSignature)));
 	}
 
 	void DX12GraphicsRenderer::PopulateCommandList()
 	{
 		// Command list allocators can only be reset when the associated 
-// command lists have finished execution on the GPU; apps should use 
-// fences to determine GPU execution progress.
+		// command lists have finished execution on the GPU; apps should use 
+		// fences to determine GPU execution progress.
 		ThrowIfFailed(m_commandAllocator->Reset());
 
 		// However, when ExecuteCommandList() is called on a particular command 
@@ -325,10 +412,18 @@ namespace Dune
 		m_commandList->RSSetScissorRects(1, &m_scissorRect);
 
 		// Indicate that the back buffer will be used as a render target.
-		auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
-		m_commandList->ResourceBarrier(1, &barrier);
+		D3D12_RESOURCE_BARRIER renderTargetBarrier;
+		renderTargetBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		renderTargetBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+		renderTargetBarrier.Transition.pResource = m_renderTargets[m_frameIndex].Get();
+		renderTargetBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+		renderTargetBarrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		renderTargetBarrier.Transition.Subresource =
+			D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		m_commandList->ResourceBarrier(1, &renderTargetBarrier);
 
-		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+		rtvHandle.ptr = rtvHandle.ptr + (m_frameIndex * m_rtvDescriptorSize);
 		m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
 
 		// Record commands.
@@ -339,8 +434,14 @@ namespace Dune
 		m_commandList->DrawInstanced(3, 1, 0, 0);
 
 		// Indicate that the back buffer will now be used to present.
-		barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
-		m_commandList->ResourceBarrier(1, &barrier);
+		D3D12_RESOURCE_BARRIER presentBarrier;
+		presentBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		presentBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+		presentBarrier.Transition.pResource = m_renderTargets[m_frameIndex].Get();
+		presentBarrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		presentBarrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+		presentBarrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		m_commandList->ResourceBarrier(1, &presentBarrier);
 
 		ThrowIfFailed(m_commandList->Close());
 	}
