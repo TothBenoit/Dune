fa45a3a06d5980a639706c84333c0479d28bc5c5
TOTH Benoit <47602460+Ahzenrei@users.noreply.github.com>
2023-03-26 01:29:08 +0100

    Remove nugget package from launcher and test projects

--- DuneLauncher/DuneLauncher.cpp
+++ DuneLauncher/DuneLauncher.cpp
@@ -1,12 +1,5 @@
 #include <Dune.h>
 
-extern "C"
-{
-	// Used to enable the "Agility SDK" components
-	__declspec(dllexport) extern const uint32_t D3D12SDKVersion = 608 /* D3D12_SDK_VERSION*/;
-	__declspec(dllexport) extern const char* D3D12SDKPath = reinterpret_cast<const char*>(u8".\\D3D12\\");
-}
-
 class DuneLauncher : public Dune::Application
 {
 public:
--- DuneTest/DuneTest.cpp
+++ DuneTest/DuneTest.cpp
@@ -1,14 +1,6 @@
 #include <Dune.h>
 #include <DirectXMath.h>
 
-extern "C"
-{
-	// Used to enable the "Agility SDK" components
-	__declspec(dllexport) extern const uint32_t D3D12SDKVersion = 608 /* D3D12_SDK_VERSION*/;
-	__declspec(dllexport) extern const char* D3D12SDKPath = reinterpret_cast<const char*>(u8".\\D3D12\\");
-}
-
-
 using namespace DirectX;
 
 class DuneLauncher : public Dune::Application
