54bf8561677f40a2ca5ee343afef8a0d53a519a7
TothBenoit <benoit.tothdenimal@gmail.com>
2024-01-19 14:14:01 +0100

    Added cube mesh to test

--- DuneTest/DuneTest.cpp
+++ DuneTest/DuneTest.cpp
@@ -6,13 +6,60 @@ std::mutex g_mutex; // API is not thread-safe yet
 
 using namespace Dune;
 
-void MovingTriangle(Graphics::Device* pDevice)
+struct Vertex
 {
-	g_mutex.lock();
-	Graphics::View* pView{ Graphics::CreateView({.pDevice = pDevice}) };
+	dVec3 vPos;
+	dVec3 vNormal;
+};
+
+static const dU16 cubeIndices[]
+{
+	0, 1, 2, 0, 2, 3,			//Face
+	4, 6, 5, 4, 7, 6,			//Back
+	10, 11, 9, 10, 9, 8,		//Left
+	13, 12, 14, 13, 14, 15,		//Right
+	16, 18, 19, 16, 19, 17,		//Top
+	22, 20, 21, 22, 21, 23		//Bottom
+};
+
+static const Vertex cubeVertices[] =
+{
+	{ {-0.5f, -0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f } }, // 0
+	{ {-0.5f,  0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f } }, // 1
+	{ { 0.5f,  0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f } }, // 2
+	{ { 0.5f, -0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f } }, // 3
+
+	{ {-0.5f, -0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f } }, // 4
+	{ {-0.5f,  0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f } }, // 5
+	{ { 0.5f,  0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f } }, // 6
+	{ { 0.5f, -0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f } }, // 7
+
+	{ {-0.5f, -0.5f, -0.5f}, {-1.0f,  0.0f,  0.0f } }, // 8
+	{ {-0.5f,  0.5f, -0.5f}, {-1.0f,  0.0f,  0.0f } }, // 9
+	{ {-0.5f, -0.5f,  0.5f}, {-1.0f,  0.0f,  0.0f } }, // 10
+	{ {-0.5f,  0.5f,  0.5f}, {-1.0f,  0.0f,  0.0f } }, // 11
+
+	{ { 0.5f,  0.5f, -0.5f}, { 1.0f,  0.0f,  0.0f } }, // 12
+	{ { 0.5f, -0.5f, -0.5f}, { 1.0f,  0.0f,  0.0f } }, // 13
+	{ { 0.5f,  0.5f,  0.5f}, { 1.0f,  0.0f,  0.0f } }, // 14
+	{ { 0.5f, -0.5f,  0.5f}, { 1.0f,  0.0f,  0.0f } }, // 15
+
+	{ {-0.5f,  0.5f, -0.5f}, { 0.0f,  1.0f,  0.0f } }, // 16
+	{ { 0.5f,  0.5f, -0.5f}, { 0.0f,  1.0f,  0.0f } }, // 17
+	{ {-0.5f,  0.5f,  0.5f}, { 0.0f,  1.0f,  0.0f } }, // 18
+	{ { 0.5f,  0.5f,  0.5f}, { 0.0f,  1.0f,  0.0f } }, // 19
+
+	{ {-0.5f, -0.5f, -0.5f}, { 0.0f, -1.0f,  0.0f } }, // 20
+	{ { 0.5f, -0.5f, -0.5f}, { 0.0f, -1.0f,  0.0f } }, // 21
+	{ {-0.5f, -0.5f,  0.5f}, { 0.0f, -1.0f,  0.0f } }, // 22
+	{ { 0.5f, -0.5f,  0.5f}, { 0.0f, -1.0f,  0.0f } }, // 23
+};
+
+Handle<Graphics::Pipeline> CreateTrianglePipeline(Graphics::Device* pDevice)
+{
 	const wchar_t* args[] = { L"-Zi" };
 
-	Handle<Graphics::Shader> vertexShader =
+	Handle<Graphics::Shader> triVertexShader =
 		Graphics::CreateShader
 		({
 			.stage = Graphics::EShaderStage::Vertex,
@@ -22,7 +69,7 @@ void MovingTriangle(Graphics::Device* pDevice)
 			.argsCount = _countof(args),
 			});
 
-	Handle<Graphics::Shader> pixelShader =
+	Handle<Graphics::Shader> triPixelShader =
 		Graphics::CreateShader
 		({
 			.stage = Graphics::EShaderStage::Pixel,
@@ -32,11 +79,11 @@ void MovingTriangle(Graphics::Device* pDevice)
 			.argsCount = _countof(args),
 			});
 
-	Handle<Graphics::Pipeline> pipeline =
+	Handle<Graphics::Pipeline> triPipeline =
 		Graphics::CreateGraphicsPipeline
 		({
-			.vertexShader = vertexShader,
-			.pixelShader = pixelShader,
+			.vertexShader = triVertexShader,
+			.pixelShader = triPixelShader,
 			.bindingLayout =
 			{
 				.slots =
@@ -51,16 +98,30 @@ void MovingTriangle(Graphics::Device* pDevice)
 			.pDevice = pDevice
 			});
 
-	dU16 indices[]{ 0, 1, 2 };
-	dVec3 vertices[]{ { -0.5f, -0.5f, 0.0 }, { 0.0f, 0.5f, 0.0 }, { 0.5f, -0.5f, 0.0 } };
-	Handle<Graphics::Mesh> triangle = Graphics::CreateMesh(pView, indices, 3, vertices, 3, sizeof(dVec3));
+	Graphics::ReleaseShader(triPixelShader);
+	Graphics::ReleaseShader(triVertexShader);
+
+	return triPipeline;
+}
+
+void Test(Graphics::Device* pDevice)
+{
+	g_mutex.lock();
+	Graphics::View* pView{ Graphics::CreateView({.pDevice = pDevice}) };
+
+	Handle<Graphics::Pipeline> triPipeline = CreateTrianglePipeline(pDevice);
+
+	dU16 triIndices[]{ 0, 1, 2 };
+	dVec3 triVertices[]{ { -0.5f, -0.5f, 0.0 }, { 0.0f, 0.5f, 0.0 }, { 0.5f, -0.5f, 0.0 } };
+	Handle<Graphics::Mesh> triangle = Graphics::CreateMesh(pView, triIndices, 3, triVertices, 3, sizeof(dVec3));
+	Handle<Graphics::Mesh> cube = Graphics::CreateMesh(pView, cubeIndices, 3, cubeVertices, 3, sizeof(Vertex));
 
 	Graphics::Command* pCommand =
 		Graphics::CreateCommand
 		({
 			.type = Graphics::ECommandType::Graphics,
 			.pView = pView
-			});
+		});
 
 	const Graphics::Mesh& mesh = Graphics::GetMesh(triangle);
 
@@ -89,7 +150,7 @@ void MovingTriangle(Graphics::Device* pDevice)
 		Graphics::BeginFrame(pView);
 		Graphics::MapBuffer(colorBuff, color, 16);
 		Graphics::ResetCommand(pCommand);
-		Graphics::SetPipeline(pCommand, pipeline);
+		Graphics::SetPipeline(pCommand, triPipeline);
 		Graphics::SetRenderTarget(pCommand, pView);
 		Graphics::ClearRenderTarget(pCommand, pView);
 		Graphics::PushGraphicsConstants(pCommand, 0, offset, 16);
@@ -104,10 +165,9 @@ void MovingTriangle(Graphics::Device* pDevice)
 
 	g_mutex.lock();
 	Graphics::ReleaseBuffer(colorBuff);
+	Graphics::ReleaseMesh(cube);
 	Graphics::ReleaseMesh(triangle);
-	Graphics::ReleaseShader(pixelShader);
-	Graphics::ReleaseShader(vertexShader);
-	Graphics::ReleasePipeline(pipeline);
+	Graphics::ReleasePipeline(triPipeline);
 	Graphics::DestroyCommand(pCommand);
 	Graphics::DestroyView(pView);
 	g_mutex.unlock();
@@ -127,7 +187,7 @@ int main(int argc, char** argv)
 
 	for (dU32 i{ 0 }; i < testCount; i++)
 	{
-		tests.emplace_back(std::thread(&MovingTriangle, pDevice));
+		tests.emplace_back(std::thread(&Test, pDevice));
 	}
 
 	for (dU32 i{ 0 }; i < testCount; i++)
